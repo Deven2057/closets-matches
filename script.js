e8a003a9ba951cfd83a8878b43b9daa912d647c6
Devendra Bhattarai <91841677+Deven2057@users.noreply.github.com>
2023-05-10 17:34:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ let id1= document.getElementById("id1")
 console.log(id1)
 console.log(id1.matches(".class"))
 console.log(id1.matches(".box"))
+
+
+let sp1= document.getElementById("sp1")
+console.log(sp1.closest(".box"))
+console.log(sp1.closest("#sp1"))
+console.log(id1.contains(sp1))
